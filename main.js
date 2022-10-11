314db41aa07e428b8248c1b9bf735025bc2a5c78
nelsondev <sandoval19371@gmail.com>
2022-10-11 03:00:30 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function mision (variable) {
+let parrafo = document.getElementById("divparrafo");
+
+
+if (variable === 'mision') {
+    parrafo.innerText= 'Veniam nostrud cillum dolore adipisicing enim et aliqua fugiat tempor.Veniam nostrud cillum dolore adipisicing enim et aliqua fugiat tempor.Veniam nostrud cillum dolore adipisicing enim et aliqua fugiat tempor.Veniam nostrud cillum dolore adipisicing enim et aliqua fugiat tempor.Veniam nostrud cillum dolore adipisicing enim et aliqua fugiat tempor.'
+}
+
+else{
+    if(variable === 'vision'){
+        parrafo.innerText= 'Veniam nostrud cillum dolore adipisicing enim et aliqua fugiat tempor.Veniam nostrud cillum dolor<e adipisicing enim et aliqua fugiat tempor.Veniam nostrud cillum<br> dolore adipisicing enim et aliqua fugiat tempor.Veniam nostrud cillum dolore adipisicing enim et aliqua fugiat tempor.Veniam nostrud cillum dolore adipisicing enim et aliqua fugiat tempor234124412412412.'
+    }
+}
+
+
+}
